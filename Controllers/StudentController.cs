4af46f35f377de984d3fb18dbecb577b44ccece6
HenrickyL <HenrickyL1@gmail.com>
2024-08-19 15:51:39 -0300

    initial controller

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StudentApi.Controllers;
+
+
+[Route("api/students")]
+[ApiController]
+public class StudentController : Controller
+{
+    [HttpGet]
+    [ProducesResponseType(typeof(List<Object>), 200)]
+    public ActionResult<List<Object>> Get() { 
+        return new List<Object>();
+    }
+}
